c1ebc07c5e9cbcc6a2c123e48ec57fafbcc48649
Joel Palmer <joelpalmer@gmail.com>
2018-07-10 15:02:46 -0500

    finish guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate rand;
 
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!!!");
@@ -10,11 +11,28 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess) //refs are also immutable by default
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess) //refs are also immutable by default
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
 }
